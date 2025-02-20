1bf2837ed7f0bedb406a4c271911cb0056f6eaf3
lylyl2 <juenter7396@gmail.com>
2025-02-20 18:00:50 +0900

    commiiiiiit

--- workspace/JSPBasic/src/main/java/com/servlet/MyServlet.java
+++ workspace/JSPBasic/src/main/java/com/servlet/MyServlet.java
@@ -1,6 +1,7 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,7 +22,15 @@ public class MyServlet extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("myServlet 실행됨");
+		//System.out.println("myServlet 실행됨");
+		
+		//브라우저로의 출력 - out 객체
+		//서버가 브라우저로 응답을 보낼 때, 이 문서는 어떤 컨텐츠 내용인지를 작성해줘야 함.
+		//컨텐츠타입(mime타입)
+		resp.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.println("<b>안녕하세요!</b>");
+		
 	}
 	
 	@Override
---
+++ workspace/JSPBasic/src/main/webapp/request/req_post_ex02.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+	//post 방식의 한글처리
+	request.setCharacterEncoding("utf-8");
+	
+	String id = request.getParameter("id");
+	String pw = request.getParameter("pw");
+	String name = request.getParameter("name");
+
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	
+	<%=id %><br>
+	<%=pw %><br>
+	<%=name %><br>
+	
+</body>
+</html>
